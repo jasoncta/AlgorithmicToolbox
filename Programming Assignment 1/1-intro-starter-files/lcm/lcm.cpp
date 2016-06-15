d8194f0c73db9e434f4a0041ff599c12bc32a091
Jason Tan <jasoncta@usc.edu>
2016-06-14 22:33:47 -0700

    completed lcm

--- Programming Assignment 1/1-intro-starter-files/lcm/lcm.cpp
+++ Programming Assignment 1/1-intro-starter-files/lcm/lcm.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 
-long long lcm(int a, int b) {
-  //write your code here
-  return a*b;
+long long EuclidGCD(long long a, long long b) {
+  if (b == 0) {
+    return a;
+  }
+  long long aa = a%b;
+  return EuclidGCD(b, aa);
 }
 
+
+long long lcm(long long a, long long b) {
+  //write your code here
+  return (a*b)/EuclidGCD(a, b);
+}
+
+
 int main() {
-  int a, b;
+  long long a, b;
   std::cin >> a >> b;
   std::cout << lcm(a, b) << std::endl;
   return 0;
