9af678bc27c0909e12a4859721b58f990b1168c6
Jason Tan <jasoncta@usc.edu>
2016-06-21 19:10:15 -0700

    dot product

--- Programming Assignment 2/02_greedy_algorithms_starter_files/dot_product/dot_product.cpp
+++ Programming Assignment 2/02_greedy_algorithms_starter_files/dot_product/dot_product.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <math.h> 
 
 using std::vector;
 
@@ -8,7 +9,34 @@ long long min_dot_product(vector<int> a, vector<int> b) {
   // write your code here
   long long result = 0;
   for (size_t i = 0; i < a.size(); i++) {
-    result += a[i] * b[i];
+    int max = pow(-11, 5);
+    int min = pow(11, 5);
+    size_t indexA = 0;
+    size_t indexB = 0;
+
+    for (size_t j = 0; j < a.size(); j++) {
+      int tempA = a[j];
+      int tempB = b[j];
+
+      if (tempA > max) {
+        max = tempA;
+        //std::cout << "tempA" << tempA << "max" << max << std::endl;
+        indexA = j;
+      }
+
+      if (tempB < min) {
+        min = tempB;
+        //std::cout << "tempB" << tempB << "min" << min << std::endl;
+        indexB = j;
+      }
+    }
+
+    result += a[indexA] * b[indexB];
+
+    a[indexA] = pow(-11,5);
+    b[indexB] = pow(11,5);
+
+    //result += a[indexA] * b[indexB];
   }
   return result;
 }
