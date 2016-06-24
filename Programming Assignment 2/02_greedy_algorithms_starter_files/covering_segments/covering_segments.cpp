4a4ac5b46bc15fc03ad3757905832ad257658719
Jason Tan <jasoncta@usc.edu>
2016-06-23 22:30:27 -0700

    covering segments

--- Programming Assignment 2/02_greedy_algorithms_starter_files/covering_segments/covering_segments.cpp
+++ Programming Assignment 2/02_greedy_algorithms_starter_files/covering_segments/covering_segments.cpp
@@ -9,13 +9,29 @@ struct Segment {
   int start, end;
 };
 
+bool segmentComparator(Segment a , Segment b) {
+  return a.end < b.end;
+}
+
 vector<int> optimal_points(vector<Segment> &segments) {
   vector<int> points;
   //write your code here
-  for (size_t i = 0; i < segments.size(); ++i) {
-    points.push_back(segments[i].start);
-    points.push_back(segments[i].end);
+std::sort(segments.begin(), segments.end(), segmentComparator);
+
+  int endIndex = segments[0].end;
+  for (size_t i = 1; i < segments.size(); ++i) {
+    if (segments[i].start > endIndex) {
+      points.push_back(endIndex);
+      endIndex = segments[i].end;
+    }
+    else {
+      if (segments[i].end < endIndex)
+      endIndex = segments[i].end;
+    }
+    //points.push_back(segments[i].start);
+    //points.push_back(segments[i].end);
   }
+  points.push_back(endIndex);
   return points;
 }
 
